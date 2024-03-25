5db74b099bffdfa0d32e335f95b3f7cdd8015c92
tychen742 <tychen742@gmail.com>
2024-03-25 12:28:26 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+project = 'python'
+copyright = '2024, tychen'
+author = 'tychen'
+release = '.01'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'furo'
+# html_static_path = ['_static']
